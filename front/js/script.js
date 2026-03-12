45705b9e9350eb0b7f1bf58cdc3891815b8f7ce2
Cesar_Desktop <162651959+Guilherme-Cesar-dev@users.noreply.github.com>
2026-03-12 16:37:46 -0300

    Library edit, new books, footer and input type file .pdf

--- front/js/script.js
+++ front/js/script.js
@@ -6,3 +6,20 @@ function a(){
     console.log(o)
     console.log(b)
 }
+
+function addBook(){
+    //AQUI TEM Q TER A OPÇÃO DE POPUP, pra o user colocar o .pdf
+}
+
+const opeen = document.getElementById("addBook")
+const closee = document.getElementById("closee")
+const modal = document.getElementById("input-file")
+
+opeen.addEventListener("click", ()=> {
+    modal.classList.add("open");
+});
+
+closee.addEventListener("click", ()=> {
+    modal.classList.remove("open");
+    console.log(1)
+});
